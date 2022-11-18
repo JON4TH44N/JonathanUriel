8356e0dfe7f705523e1ef11237866a257f37656a
JON4TH44N <118494434+JON4TH44N@users.noreply.github.com>
2022-11-17 19:35:04 -0600

    Add files via upload

---
+++ syndicai.py
@@ -0,0 +1,33 @@
+from IPython.core.display import Image
+import os, cv2
+import numpy as np
+
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+
+from utils import url_to_image, b64_to_image, image_to_base64
+from PIL import image
+
+args = {
+    "perrosygatos": "clasificador",
+    "model": "RedCNN_PerrosyGatos.h5",
+}
+
+class PythonPredictor:
+  def _init_(self,config):
+    print("[INFO] cargando modelo entrenado...")
+    self.model = load_model(args["model"])
+
+  def predict(self, payload):
+    try:
+      image = Image.open(payload["image"].file)
+    except:
+      image = Image.open(payload["image"].file)
+      orig = image.copy()
+      (h, w) = image.shape[:2]
+
+      img_tensor = img_to_array(image)
+      img_tensor = np.expend.dims(img_tensor, axis=0)
+      img_tensor /= 255
+      return self.model.predict(img_tensor)[0][0]
